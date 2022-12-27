646cb9358fa06c411b9aef06efece812d70e4e18
Sabal Shrestha <sabalshrestha@Sabals-MacBook-Pro.local>
2022-12-27 14:46:49 +0545

    seo optimize

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-(function (d, w, c) {
-  w.ChatraID = "zECfQ5CCWmujfp2rE";
-  var s = d.createElement("script");
-  w[c] =
-    w[c] ||
-    function () {
-      (w[c].q = w[c].q || []).push(arguments);
-    };
-  s.async = true;
-  s.src = "https://call.chatra.io/chatra.js";
-  if (d.head) d.head.appendChild(s);
-})(document, window, "Chatra");
-
 window.onscroll = function () {
   if (window.scrollY > 30) {
     document.getElementById("navbar").style.backgroundColor = "#002480";
